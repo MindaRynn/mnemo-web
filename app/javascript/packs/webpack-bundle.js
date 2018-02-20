9554adc4eda14d9f98a4f5c1e0604ea6a05ef332
Makhamwan <mkhv.bt96@gmail.com>
2018-02-20 13:31:18 +0700

    sent link to reset password, front-end

--- app/javascript/packs/webpack-bundle.js
+++ app/javascript/packs/webpack-bundle.js
@@ -3,10 +3,12 @@ import ReactOnRails from 'react-on-rails';
 import LandingPage from '../bundles/MnemoFrontend/components/LandingPage';
 import LoginForm from '../bundles/MnemoFrontend/components/devise/sessions/LoginForm';
 import RegistrationForm from '../bundles/MnemoFrontend/components/devise/registrations/RegistrationForm';
+import ForgotPasswordForm from '../bundles/MnemoFrontend/components/devise/passwords/ForgotPasswordForm';
 
 // This is how react_on_rails can see the MnemoFrontend in the browser.
 ReactOnRails.register({
   LandingPage,
   LoginForm,
-  RegistrationForm
+  RegistrationForm,
+  ForgotPasswordForm
 });
