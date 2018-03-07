4c3569097143693de04f02ade6d9cc5ed661756c
MindaRyn <natcha.p@ku.th>
2018-03-07 08:55:25 +0700

    create weak entity for user and chatroom

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221091057) do
+ActiveRecord::Schema.define(version: 20180307014754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20180221091057) do
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.text "name"
+    t.string "room_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
