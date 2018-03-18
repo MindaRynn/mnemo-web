68486d06a06c32c8929c2fc930431276e55d8773
MindaRyn <natcha.p@ku.th>
2018-03-19 01:13:11 +0700

    Create engines for API endpoint

--- Gemfile
+++ Gemfile
@@ -58,4 +58,4 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby] # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 
-gem 'mini_racer', platforms: :ruby
+gem 'api', path: 'engines/api'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  mount Api::Engine => "/api"
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
