c29bdade1da3c03edd276cc6b1e61c285a287368
Patinya Yongyai <patinya.y@ku.th>
2018-04-03 19:30:11 +0700

    add tab for reset password and setting profile, add auto fill information in input field

--- app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
+++ app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
@@ -7,12 +7,42 @@ export default class EditForm extends React.Component {
 
     this.inputOnChangeHandler = this.inputOnChangeHandler.bind(this)
     this.submitHandler = this.submitHandler.bind(this)
+    this.handleTab = this.handleTab.bind(this)
+    this.handleChangeEmail = this.handleChangeEmail.bind(this)
+    this.handleChangeUsername = this.handleChangeUsername.bind(this)
     
     this.state = {
-      avatar: this.props.avatarUrl
+      avatar: this.props.avatarUrl,
+      activeTab: "profile",
+      username: this.props.username,
+      email: this.props.email
     }
   }
 
+  handleTab(e) {
+    e.preventDefault();
+    const tab = e.target.name
+    this.setState(() => {
+      return {
+        activeTab: tab
+      };
+    });
+  }
+
+  handleChangeEmail(e) {
+    e.preventDefault();
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  handleChangeUsername(e) {
+    e.preventDefault();
+    this.setState({
+      username: e.target.value
+    });
+  }
+ 
   handleClick(e) {
     e.preventDefault()
     // Explicitly focus the text input using the raw DOM API.
@@ -52,34 +82,98 @@ export default class EditForm extends React.Component {
     // debugger
     return (
       <div>
-        {avatar ? <Image type="standard" src={this.state.avatar} /> : <Image type="standard" src="https://storage.googleapis.com/mnemo-storage/placeHolderAvatar/tempAvatar.jpg" />}
-        <form id="avatarForm" onSubmit={this.submitHandler} encType="multipart/form-data">
-          <h3>Change profile picture</h3>
-          <div className="form-input-group">
-            <label htmlFor="name">Name</label>
-            <input name="user[name]"/>
-          </div>
-          <input onChange={this.inputOnChangeHandler} id="avatar" type="file" name="file" />
-          <input id="submitButton" style={{display: 'none'}} type="submit" value="Upload" />
-        </form>
-
-        <form ref={(ref) => this.avtarForm = ref} action={url} method="post" autoComplete="off">
-          <div className="form-input-group">
-            <label htmlFor="name">Name</label>
-            <input name="user[name]"/>
-          </div>
-
-          <input type="hidden" name="authenticity_token" value={csrfToken} />
-          <input type="hidden" name="_method" value="put"/>
-          <input type="hidden" name="user[image]" value={this.state.avatar} />
-
-          <div className="form-input-group flex-end">
-            <div className="form-group centered">
-              <button onClick={e => {this.handleClick}} className="btn btn-primary" type="submit">Confirm</button>
+        <div className="setting-container">
+          <h1 className="setting-header">Setting</h1>
+          <div className="row">
+            <div className="col-md-3">
+              <div className="row">
+                <button style={this.state.activeTab == "profile" ? {color: '#454550'} : {color: '#d7d7d7'}} name="profile" onClick={this.handleTab} className="btn btn-tab">Profile</button>
+              </div>
+              <div className="row">
+                <button style={this.state.activeTab == "password" ? {color: '#454550'} : {color: '#d7d7d7'}} name="password" onClick={this.handleTab} className="btn btn-tab">Password</button>
+              </div>
             </div>
-          </div>
+            {this.state.activeTab == "profile" && 
+              <div className="col-md-6">
+                {avatar ? <Image type="standard" src={this.state.avatar} /> : <Image type="standard" src="https://storage.googleapis.com/mnemo-storage/placeHolderAvatar/tempAvatar.jpg" />}
+                <form id="avatarForm" onSubmit={this.submitHandler} encType="multipart/form-data">
+                  <input onChange={this.inputOnChangeHandler} id="avatar" type="file" name="file" />
+                  <input id="submitButton" style={{display: 'none'}} type="submit" value="Upload" />
+                </form>
 
-        </form>
+                <form ref={(ref) => this.avtarForm = ref} action={url} method="post" autoComplete="off">
+                  <div className="form-input-group">
+                    <div className="row">
+                      <div className="col-md-4">
+                        <label htmlFor="username">Username</label>
+                      </div>
+                      <div className="col-md-8">
+                        <input className="input input-width-max" onChange={this.handleChangeUsername} name="user[name]" value={this.state.username}/>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-4">
+                        <label htmlFor="email">Email</label>
+                      </div>
+                      <div className="col-md-8">
+                        <input className="input input-width-max" onChange={this.handleChangeEmail} name="user[email]" value={this.state.email}/>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-4"></div>
+                      <div className="col-md-8">
+                        <button onClick={e => {this.handleClick}} className="btn btn-primary btn-max-width" type="submit">Save change</button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <input type="hidden" name="authenticity_token" value={csrfToken} />
+                  <input type="hidden" name="_method" value="put"/>
+                  <input type="hidden" name="user[image]" value={this.state.avatar} />
+
+                </form>
+              </div>
+            }
+
+            {this.state.activeTab == "password" && 
+              <div className="col-md-6">
+                <div className="form-input-group">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <label htmlFor="username">Current sassword</label>
+                    </div>
+                    <div className="col-md-8">
+                      <input type="password" className="input input-width-max" name="user[password]"/>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-4">
+                      <label htmlFor="email">New password</label>
+                    </div>
+                    <div className="col-md-8">
+                      <input type="password" className="input input-width-max" name="user[password]"/>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-4">
+                      <label htmlFor="email">Re-enter password</label>
+                    </div>
+                    <div className="col-md-8">
+                      <input type="password" className="input input-width-max" name="user[password]"/>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-4"></div>
+                    <div className="col-md-8">
+                      <button onClick={e => {this.handleClick}} className="btn btn-primary btn-max-width" type="submit">Save change</button>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            }
+          </div>
+        </div>
+
       </div>
 
 
