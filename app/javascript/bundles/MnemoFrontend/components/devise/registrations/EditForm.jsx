50a2c9c17114190d9d64d884c1fd133eb43cb323
Patinya Yongyai <patinya.y@ku.th>
2018-04-06 14:23:34 +0700

    edit layout and add bio field

--- app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
+++ app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
@@ -10,12 +10,14 @@ export default class EditForm extends React.Component {
     this.handleTab = this.handleTab.bind(this)
     this.handleChangeEmail = this.handleChangeEmail.bind(this)
     this.handleChangeUsername = this.handleChangeUsername.bind(this)
+    this.handleChangeBio = this.handleChangeBio.bind(this)
     
     this.state = {
       avatar: this.props.avatarUrl,
       activeTab: "profile",
       username: this.props.username,
-      email: this.props.email
+      email: this.props.email,
+      bio: this.props.bio
     }
   }
 
@@ -43,6 +45,13 @@ export default class EditForm extends React.Component {
     });
   }
 
+  handleChangeBio(e) {
+    e.preventDefault();
+    this.setState({
+      bio: e.target.value
+    });
+  }
+  
   handleClick(e) {
     e.preventDefault()
     // Explicitly focus the text input using the raw DOM API.
@@ -85,7 +94,7 @@ export default class EditForm extends React.Component {
         <div className="setting-container">
           <h1 className="setting-header">Setting</h1>
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-md-2">
               <div className="row">
                 <button style={this.state.activeTab == "profile" ? {color: '#454550'} : {color: '#d7d7d7'}} name="profile" onClick={this.handleTab} className="btn btn-tab">Profile</button>
               </div>
@@ -94,51 +103,69 @@ export default class EditForm extends React.Component {
               </div>
             </div>
             {this.state.activeTab == "profile" && 
-              <div className="col-md-6">
-                {avatar ? <Image type="standard" src={this.state.avatar} /> : <Image type="standard" src="https://storage.googleapis.com/mnemo-storage/placeHolderAvatar/tempAvatar.jpg" />}
-                <form id="avatarForm" onSubmit={this.submitHandler} encType="multipart/form-data">
-                  <input onChange={this.inputOnChangeHandler} id="avatar" type="file" name="file" />
-                  <input id="submitButton" style={{display: 'none'}} type="submit" value="Upload" />
-                </form>
+              <div className="col-md-10">
+                <div className="row space-item">
 
-                <form ref={(ref) => this.avtarForm = ref} action={url} method="post" autoComplete="off">
-                  <div className="form-input-group">
-                    <div className="row">
-                      <div className="col-md-4">
-                        <label htmlFor="username">Username</label>
+                  <div className="col-md-7">
+                    <form ref={(ref) => this.avtarForm = ref} action={url} method="post" autoComplete="off">
+                      <div className="form-input-group">
+                        <div className="row">
+                          <div className="col-md-4">
+                            <label htmlFor="username">Username</label>
+                          </div>
+                          <div className="col-md-8">
+                            <input className="input input-width-max" onChange={this.handleChangeUsername} name="user[name]" value={this.state.username}/>
+                          </div>
+                        </div>
+                        <div className="row space-item">
+                          <div className="col-md-4">
+                            <label htmlFor="email">Email</label>
+                          </div>
+                          <div className="col-md-8">
+                            <input className="input input-width-max" onChange={this.handleChangeEmail} name="user[email]" value={this.state.email}/>
+                          </div>
+                        </div>
+                        <div className="row space-item">
+                          <div className="col-md-4">
+                            <label htmlFor="bio">Bio</label>
+                          </div>
+                          <div className="col-md-8">
+                            <input className="input input-width-max" onChange={this.handleChangeBio} name="user[bio]" value={this.state.bio}/>
+                          </div>
+                        </div>
+                        <div className="row space-item">
+                          <div className="col-md-4"></div>
+                          <div className="col-md-8">
+                            <button onClick={e => {this.handleClick}} className="btn btn-primary btn-max-width" type="submit">Save change</button>
+                          </div>
+                        </div>
                       </div>
-                      <div className="col-md-8">
-                        <input className="input input-width-max" onChange={this.handleChangeUsername} name="user[name]" value={this.state.username}/>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-4">
-                        <label htmlFor="email">Email</label>
-                      </div>
-                      <div className="col-md-8">
-                        <input className="input input-width-max" onChange={this.handleChangeEmail} name="user[email]" value={this.state.email}/>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-4"></div>
-                      <div className="col-md-8">
-                        <button onClick={e => {this.handleClick}} className="btn btn-primary btn-max-width" type="submit">Save change</button>
-                      </div>
-                    </div>
+
+                      <input type="hidden" name="authenticity_token" value={csrfToken} />
+                      <input type="hidden" name="_method" value="put"/>
+                      <input type="hidden" name="user[image]" value={this.state.avatar} />
+
+                    </form>
                   </div>
 
-                  <input type="hidden" name="authenticity_token" value={csrfToken} />
-                  <input type="hidden" name="_method" value="put"/>
-                  <input type="hidden" name="user[image]" value={this.state.avatar} />
+                  <div className="col-md-2"><div className="line-split"></div></div>
 
-                </form>
+                  <div className="col-md-3">
+                    {avatar ? <Image type="standard" src={this.state.avatar} /> : <Image type="standard" src="https://storage.googleapis.com/mnemo-storage/placeHolderAvatar/tempAvatar.jpg" />}
+                    <form id="avatarForm" onSubmit={this.submitHandler} encType="multipart/form-data">
+                      <label htmlFor="avatar" style={{marginBottom: "0px"}} className="btn btn-select-image space-item">Select image</label>
+                      <input onChange={this.inputOnChangeHandler} style={{display: 'none'}} id="avatar" type="file" name="file" />
+                      <input id="submitButton" style={{display: 'none'}} type="submit" value="Upload" />
+                    </form>
+                  </div>
+                </div>
               </div>
             }
 
             {this.state.activeTab == "password" && 
               <div className="col-md-6">
                 <div className="form-input-group">
-                  <div className="row">
+                  <div className="row space-item">
                     <div className="col-md-4">
                       <label htmlFor="username">Current sassword</label>
                     </div>
@@ -146,7 +173,7 @@ export default class EditForm extends React.Component {
                       <input type="password" className="input input-width-max" name="user[password]"/>
                     </div>
                   </div>
-                  <div className="row">
+                  <div className="row space-item">
                     <div className="col-md-4">
                       <label htmlFor="email">New password</label>
                     </div>
@@ -154,7 +181,7 @@ export default class EditForm extends React.Component {
                       <input type="password" className="input input-width-max" name="user[password]"/>
                     </div>
                   </div>
-                  <div className="row">
+                  <div className="row space-item">
                     <div className="col-md-4">
                       <label htmlFor="email">Re-enter password</label>
                     </div>
@@ -162,7 +189,7 @@ export default class EditForm extends React.Component {
                       <input type="password" className="input input-width-max" name="user[password]"/>
                     </div>
                   </div>
-                  <div className="row">
+                  <div className="row space-item">
                     <div className="col-md-4"></div>
                     <div className="col-md-8">
                       <button onClick={e => {this.handleClick}} className="btn btn-primary btn-max-width" type="submit">Save change</button>
