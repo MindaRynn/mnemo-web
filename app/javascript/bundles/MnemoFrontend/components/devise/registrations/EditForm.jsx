e7f56bcd73acf68dd3fde98999dff2750fd3664f
MindaRyn <natcha.p@ku.th>
2018-04-10 04:09:44 +0700

    Fix toggle issue

--- app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
+++ app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
@@ -27,7 +27,6 @@ export default class EditForm extends React.Component {
   //   });
   // }
 
-
   render() {
     let {url, csrfToken, signinPath, resetPasswordToken} = this.props;
     let {avatar} = this.state;
@@ -40,19 +39,14 @@ export default class EditForm extends React.Component {
           <div className="row">
             <div className="col-md-2">
               <div className="row">
-                {/* <button style={this.state.activeTab == "profile" ? {color: '#454550'} : {color: '#d7d7d7'}} name="profile" onClick={this.handleTab} className="btn btn-tab">Profile</button> */}
-                <a data-toggle="tab" href="#menu1" className="active show">Profile</a>
-                
-              </div>
-              <div className="row">
-                <a data-toggle="tab" href="#menu2">Reset Password</a>
-                {/* <button style={this.state.activeTab == "password" ? {color: '#454550'} : {color: '#d7d7d7'}} name="password" onClick={this.handleTab} className="btn btn-tab">Password</button> */}
+                <ul className="nav">
+                  <li><a data-toggle="tab" href="#menu1" className="active show">Profile</a></li>
+                  <li><a data-toggle="tab" href="#menu2">Reset Password</a></li>
+                </ul>
               </div>
             </div>
             <div className="col-md-10">
               <ProfileForm csrfToken={csrfToken} email={this.state.email}  avatar={this.state.avatar} url={url} username={this.state.username} bio={this.state.bio}/>
-            </div>
-            <div className="col-md-6">
               <PasswordForm url={url} csrfToken={csrfToken} resetPasswordToken={resetPasswordToken}/>
             </div>
           </div>
