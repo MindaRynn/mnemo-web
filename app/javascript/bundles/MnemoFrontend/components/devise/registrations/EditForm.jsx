7590d721501f2c63ad38ecf07b0387ca747ccf24
Patinya Yongyai <patinya.y@ku.th>
2018-04-10 17:02:35 +0700

    fix data-toggle and style

--- app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
+++ app/javascript/bundles/MnemoFrontend/components/devise/registrations/EditForm.jsx
@@ -6,26 +6,14 @@ import PasswordForm from './PasswordForm'
 export default class EditForm extends React.Component {
   constructor(props) {
     super(props);
-    // this.handleTab = this.handleTab.bind(this)
     
     this.state = {
       avatar: this.props.avatarUrl,
-      // activeTab: "profile",
       username: this.props.username,
       email: this.props.email,
       bio: this.props.bio
     }
   }
-
-  // handleTab(e) {
-  //   e.preventDefault();
-  //   const tab = e.target.name
-  //   this.setState(() => {
-  //     return {
-  //       activeTab: tab
-  //     };
-  //   });
-  // }
 
   render() {
     let {url, csrfToken, signinPath, resetPasswordToken} = this.props;
@@ -40,15 +28,13 @@ export default class EditForm extends React.Component {
             <div className="col-md-2">
               <div className="row">
                 <ul className="nav">
-                  <li><a data-toggle="tab" href="#menu1" className="active show">Profile</a></li>
-                  <li><a data-toggle="tab" href="#menu2">Reset Password</a></li>
+                  <li className="space-item"><a data-toggle="tab" href="#menu1" className="active show">Profile</a></li>
+                  <li className="space-item"><a data-toggle="tab" href="#menu2">Password</a></li>
                 </ul>
               </div>
             </div>
             <div className="col-md-10">
               <ProfileForm csrfToken={csrfToken} email={this.state.email}  avatar={this.state.avatar} url={url} username={this.state.username} bio={this.state.bio}/>
-            </div>
-            <div className="col-md-6">
               <PasswordForm url={url} csrfToken={csrfToken} resetPasswordToken={resetPasswordToken}/>
             </div>
           </div>
--- app/javascript/bundles/MnemoFrontend/components/devise/registrations/PasswordForm.jsx
+++ app/javascript/bundles/MnemoFrontend/components/devise/registrations/PasswordForm.jsx
@@ -14,55 +14,58 @@ export default class PasswordForm extends React.Component {
 
     return (
       <div id="menu2" className="tab-pane fade">
-        <form action={url} method="post" autoComplete="off">
-          <div className="form-input-group">
-            <div className="row space-item">
-              <div className="col-md-4 align-self-center">
-                <label htmlFor="email">New password</label>
+        <div className="col-7">
+          <form action={url} method="post" autoComplete="off">
+            <div className="form-input-group">
+              <div className="row space-item">
+                <div className="col-md-4 align-self-center">
+                  <label htmlFor="email">New password</label>
+                </div>
+                <div className="col-md-8">
+                  <input
+                    type="password"
+                    className="input input-width-max"
+                    name="user[password]"
+                  />
+                </div>
               </div>
-              <div className="col-md-8">
-                <input
-                  type="password"
-                  className="input input-width-max"
-                  name="user[password]"
-                />
+              <div className="row space-item">
+                <div className="col-md-4 align-self-center">
+                  <label htmlFor="email">Re-enter password</label>
+                </div>
+                <div className="col-md-8">
+                  <input
+                    type="password"
+                    className="input input-width-max"
+                    name="user[password_confirmation]"
+                  />
+                </div>
+              </div>
+              <input type="hidden" name="_method" value="put" />
+              <input type="hidden" name="authenticity_token" value={csrfToken} />
+              <input
+                type="hidden"
+                name="user[reset_password_token]"
+                value={resetPasswordToken}
+              />
+              <div className="row space-item">
+                <div className="col-md-4" />
+                <div className="col-md-8">
+                  <button
+                    onClick={e => {
+                      this.handleClick;
+                    }}
+                    className="btn btn-primary btn-max-width"
+                    type="submit"
+                  >
+                    Save change
+                  </button>
+                </div>
               </div>
             </div>
-            <div className="row space-item">
-              <div className="col-md-4 align-self-center">
-                <label htmlFor="email">Re-enter password</label>
-              </div>
-              <div className="col-md-8">
-                <input
-                  type="password"
-                  className="input input-width-max"
-                  name="user[password_confirmation]"
-                />
-              </div>
-            </div>
-            <input type="hidden" name="_method" value="put" />
-            <input type="hidden" name="authenticity_token" value={csrfToken} />
-            <input
-              type="hidden"
-              name="user[reset_password_token]"
-              value={resetPasswordToken}
-            />
-            <div className="row space-item">
-              <div className="col-md-4" />
-              <div className="col-md-8">
-                <button
-                  onClick={e => {
-                    this.handleClick;
-                  }}
-                  className="btn btn-primary btn-max-width"
-                  type="submit"
-                >
-                  Save change
-                </button>
-              </div>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
+        
       </div>
     );
   }
