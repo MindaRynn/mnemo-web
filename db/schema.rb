f11f8f08e04f31084892036a035dabeb1c0c974a
MindaRyn <natcha.p@ku.th>
2018-04-12 00:02:53 +0700

    edit css issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411154853) do
+ActiveRecord::Schema.define(version: 20180411162511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,8 +84,10 @@ ActiveRecord::Schema.define(version: 20180411154853) do
     t.string "name"
     t.text "image"
     t.text "bio"
+    t.bigint "time_capsule_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["time_capsule_id"], name: "index_users_on_time_capsule_id"
   end
 
 end
