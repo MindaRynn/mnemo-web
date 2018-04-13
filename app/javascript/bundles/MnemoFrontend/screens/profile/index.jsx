c7c67017c1fc79805e7b5c27016de6811dd6cd82
Patinya Yongyai <patinya.y@ku.th>
2018-04-13 17:11:10 +0700

    add data-toggle

--- app/javascript/bundles/MnemoFrontend/screens/profile/index.jsx
+++ app/javascript/bundles/MnemoFrontend/screens/profile/index.jsx
@@ -27,27 +27,39 @@ class Profile extends React.Component {
 
     return (
       <div>
-        <div className="row">
-          <div className="col-4" >
-            <center><Image type="standard" src={this.state.avatar}/></center>
+        <div className="profile-container">
+          <div className="row">
+            <div className="col-4" >
+              <center><Image type="standard" src={this.state.avatar}/></center>
+            </div>
+            <div className="col-5">
+              <div className="row"><h2>{this.state.name}</h2></div>
+              <div className="row"><p>{this.state.bio}</p></div>
+            </div>
+            <div className="col-3 text-center">
+              <PrimaryButton text="Add friends"/>
+            </div>
           </div>
-          <div className="col-5">
-            <div className="row"><h2>{this.state.name}</h2></div>
-            <div className="row"><p>{this.state.bio}</p></div>
+          <hr/>
+          <div className="row text-center">
+            <div className="col-2"></div>
+            <div className="col-4">Post <h2 className="sameline">23.5k</h2></div>
+            <div className="col-4">Friends <h2 className="sameline">235</h2></div>
+            <div className="col-2"></div>
           </div>
-          <div className="col-3 text-center">
-            <PrimaryButton text="Add friends"/>
+          <hr/>
+          <div className="input-group">
+            <input className="form-control" type="text" placeholder="Writing something.."/>
+            <div className="input-group-append">
+              <label htmlFor="file" style={{ marginBottom: "0px" }} id="upload-icon" className="btn">Upload</label>
+              <input style={{ display: "none" }} id="file" type="file" name="file"/>
+            </div>
           </div>
+          <ul className="nav">
+            <li className="space-item"><a data-toggle="tab" href="#menu1" className="space-toggle active show">All</a></li>
+            <li className="space-item"><a data-toggle="tab" href="#menu2" className="space-toggle">Opened</a></li>
+          </ul>
         </div>
-        <hr/>
-        <div className="row text-center">
-          <div className="col-2"></div>
-          <div className="col-4">Post <h2 className="sameline">23.5k</h2></div>
-          <div className="col-4">Friends <h2 className="sameline">235</h2></div>
-          <div className="col-2"></div>
-        </div>
-        <hr/>
-        <input className="w-100" type="text" placeholder="Writing something.."/>
       </div>
     );
   }
