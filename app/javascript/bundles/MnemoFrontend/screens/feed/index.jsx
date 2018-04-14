24c4582acde79fe629838392c667a73144eeb721
MindaRyn <natcha.p@ku.th>
2018-04-14 16:46:29 +0700

    Move css for comment field into module

--- app/javascript/bundles/MnemoFrontend/screens/feed/index.jsx
+++ app/javascript/bundles/MnemoFrontend/screens/feed/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const initialState = {
+  fetchedCapsule: false
+};
+
 class Feed extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = initialState;
   }
 
   componentDidMount() {
@@ -13,11 +19,28 @@ class Feed extends React.Component {
     actions.fetchTimeCapsule(currentUser.id);
   }
 
+  componentDidUpdate(prevProps) {
+    let {fetchTimeCapsuleSuccess, timeCapsules} = this.props.feed.timeCapsule
+
+    if(fetchTimeCapsuleSuccess && !prevProps.feed.timeCapsule.fetchTimeCapsuleSuccess) {
+      this.setState({
+        fetchedCapsule: true,
+      });
+    }
+  }
+
   render() {
+    let {fetchedCapsule} = this.state
 
     return (
-      <div>
-        FEED
+      <div className="row">
+        <div className="list col-4">
+          Category
+        </div>
+        {fetchedCapsule ?
+          <div className="list col-8">
+            FEED LIST
+          </div> : null }
       </div>
     );
   }
