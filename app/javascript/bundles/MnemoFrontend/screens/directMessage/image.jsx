f3033961b05a7686221a852f085b3117710e56f7
Patinya Yongyai <patinya.y@ku.th>
2018-04-15 23:13:30 +0700

    add modal for image

--- app/javascript/bundles/MnemoFrontend/screens/directMessage/image.jsx
+++ app/javascript/bundles/MnemoFrontend/screens/directMessage/image.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default class Image extends React.Component {
+
   render() {
     let {src, className} = this.props;
     const sizeStyle = {
@@ -9,8 +10,24 @@ export default class Image extends React.Component {
 
     return (
       <div className={`message-group ${className ? className : ''}`}>
-       <div className="message">
-        <img style={sizeStyle} className="img-fluid" src={src} alt=""/>
+       <div className="message text-right">
+          <div className="add-data">
+              <a href="" data-toggle="modal" data-target={(`#`+src).toString()}>
+                  <img style={sizeStyle} className="img-fluid" src={src} alt=""/>
+              </a>
+          </div>
+          <div className="modal fade" id={(src).toString()} tabIndex="-1" role="dialog" aria-labelledby={(src).toString()} aria-hidden="true">
+              <div className="modal-dialog modal-lg">
+                  <div className="modal-content">
+                      <div className="modal-body">
+                          <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button><br/>
+                          <div className="container-fluid text-center">
+                            <img src={src} className="img-fluid" alt=""/>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
        </div>
       </div>
     );
--- app/javascript/bundles/MnemoFrontend/screens/directMessage/messageWithImage.jsx
+++ app/javascript/bundles/MnemoFrontend/screens/directMessage/messageWithImage.jsx
@@ -17,7 +17,23 @@ class MessageWithImage extends React.Component {
       <div className={`message-group ${className ? className : ''}`}>
         <div className="message text-right">
           <span>{text}</span><br/>
-          <img style={sizeStyle} className="img-fluid" src={src} alt=""/>
+          <div className="add-data">
+              <a href="" data-toggle="modal" data-target={(`#`+src).toString()}>
+                  <img style={sizeStyle} className="img-fluid" src={src} alt=""/>
+              </a>
+          </div>
+          <div className="modal fade" id={(src).toString()} tabIndex="-1" role="dialog" aria-labelledby={(src).toString()} aria-hidden="true">
+              <div className="modal-dialog modal-lg">
+                  <div className="modal-content">
+                      <div className="modal-body">
+                          <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button><br/>
+                          <div className="container-fluid text-center">
+                            <img src={src} className="img-fluid" alt=""/>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
       </div>
 
