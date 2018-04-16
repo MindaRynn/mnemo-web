57696dd0d51416689374b6ef75582c209608e14c
Makhamwan <mkhv.bt96@gmail.com>
2018-04-16 15:03:35 +0700

    Category-filter

--- app/javascript/bundles/MnemoFrontend/screens/feed/categoryList.jsx
+++ app/javascript/bundles/MnemoFrontend/screens/feed/categoryList.jsx
@@ -9,8 +9,42 @@ class CategoryList extends React.Component {
 
   render() {
     return (
-      <div className="list col-3">
+      <div className="list col-3 filter-container">
         Category
+        <div className="filter-list">
+          <input type="checkbox" class="form-check-input" id=""/>
+          <label> Event </label>
+        </div>
+        <div className="filter-list">
+          <input type="checkbox" class="form-check-input" id=""/>
+          <label> Life style </label>
+        </div>
+        <div className="filter-list">
+          <input type="checkbox" class="form-check-input" id=""/>
+          <label> People </label>
+        </div>
+        <div className="filter-list" data-toggle="collapse" href="#sub-filter-list">
+          <input type="checkbox" class="form-check-input" id=""/>
+          <label> Emotion </label>
+        </div>
+        <div id="sub-filter-list" class="collapse">
+          <div className="sub-filter-list">
+            <input type="checkbox" class="form-check-input" id=""/>
+            <label> Happy </label>
+          </div>
+          <div className="sub-filter-list">
+            <input type="checkbox" class="form-check-input" id=""/>
+            <label> Love </label>
+          </div>
+          <div className="sub-filter-list">
+            <input type="checkbox" class="form-check-input" id=""/>
+            <label> Funny </label>
+          </div>
+          <div className="sub-filter-list">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+            <label> Sad </label>
+          </div>
+        </div>
       </div>
     );
   }
