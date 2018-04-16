9940ae9377389fc15b56ca1198c996ba3c71f065
MindaRyn <natcha.p@ku.th>
2018-04-17 04:29:20 +0700

    Add redirect path

--- app/javascript/bundles/MnemoFrontend/components/timeCapsuleItem/index.jsx
+++ app/javascript/bundles/MnemoFrontend/components/timeCapsuleItem/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Image from '../../components/image/';
 import moment from 'moment';
+import Link from 'react-router';
 
 import StatusCircle from '../../screens/profile/statusCircle';
 
 export default class TimeCapsuleItem extends React.Component {
   constructor(props) {
     super(props)
+    this._clickHandler = this._clickHandler.bind(this);
+  }
+
+  _clickHandler(e) {
+    let {timeCapsule} = this.props;
+    window.location = `/timeCapsule/${timeCapsule.id}`;
   }
 
   render() {
@@ -16,24 +23,22 @@ export default class TimeCapsuleItem extends React.Component {
     let wrap_date = moment(this.props.timeCapsule.wrap_date.toLocaleString()).format('LLL')
 
     return (
-      <div>
-        <div className="capsule-box">
-          <div className="avatar-container"><Image size="s" src={timeCapsule.user.image}/></div>
-          <div className="capsule-detail-container">
-            <div className="header-container">
-              <div>
-                <h3>{timeCapsule.user.name}</h3>
-                <div className="font-status-size">{created_at}</div>
-              </div>
-              <div>
-                <div>{status}<StatusCircle status={status}/></div>
-                <div>Wrapped {wrap_date}</div>
-              </div>
+      <div onClick={e => this._clickHandler(e)} className="capsule-box">
+        <div className="avatar-container"><Image size="s" src={timeCapsule.user.image}/></div>
+        <div className="capsule-detail-container">
+          <div className="header-container">
+            <div>
+              <h3>{timeCapsule.user.name}</h3>
+              <div className="font-status-size">{created_at}</div>
             </div>
-            <h3>{timeCapsule.subject}</h3>
-            <div className="">
-              {timeCapsule.memory_boxes[0].description}
+            <div>
+              <div>{status}<StatusCircle status={status}/></div>
+              <div>Wrapped {wrap_date}</div>
             </div>
+          </div>
+          <h3>{timeCapsule.subject}</h3>
+          <div className="">
+            {timeCapsule.memory_boxes[0].description}
           </div>
         </div>
       </div>
